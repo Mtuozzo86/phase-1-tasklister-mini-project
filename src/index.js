dee5be6cdec4a63841406c9706eb1330a2fd695a
Mtuozzo86 <Mtuozzo86@gmail.com>
2021-07-26 13:17:52 -0400

    working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const inputField = document.getElementById("new-task-description")
+  const toDoListBox = document.querySelector('#list ul')
+  const submitButton = document.querySelectorAll('input')[1]
+  const formArea = document.getElementById('create-task-form')
+  const li = document.createElement('li')
+  
+formArea.addEventListener('submit', function(e){
+  e.preventDefault()
+  inputField(createListItem)
+})
+
+
+  function createListItem(text){
+  const li = document.createElement('li')
+  toDoListBox.append(li)
+  li.innerText = `${text}`
+
+}
+
+
+  
 });
+
